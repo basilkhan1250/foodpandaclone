d61aaa360cb5758387b17e9d9897a7248b80c0af
M.BasilKhan <basilkhan1250@gmail.com>
2024-08-25 03:36:45 +0500

    working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import logo from './assets/foodpandalogo.png'
-import "./design.css"
+import logo from './assets/foodpandalogo.png';
+import lang from './assets/multilingual.png';
+import "./design.css";
 
 const Navbar = () => {
-
     return (
-        <div className="head">
-            <div className="start">
-                <img className="logo" src={logo} />
-                <h2>foodpanda</h2>
+        <div className="navbar">
+            <div className="navbar-left">
+                <img className="navbar-logo" src={logo} alt="Foodpanda Logo" />
+                <h2 className="navbar-brand">foodpanda</h2>
             </div>
-            <div className="end">
-                <button className="btn">Log in</button>
-                <button className="signbtn">Sign up</button>
+            <div className="navbar-right">
+                <button className="navbar-btn">Log in</button>
+                <button className="navbar-signbtn">Sign up</button>
+                <div className="navbar-right-lang">
+                    <img className="navbar-lang" src={lang} alt="Language Selector" />
+                    <p className="lang">EN</p>
+                    <select className="select ">
+                        <option ></option>
+                        <option className="english" value="English">English</option>
+                    </select>
+                    
+                </div>
+
             </div>
         </div>
-    )
+    );
+};
 
-}
-
-export default Navbar
+export default Navbar;
