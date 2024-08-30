e8b1819fe72530f9fc3f8a1b639f5b6c1810a55a
M.BasilKhan <basilkhan1250@gmail.com>
2024-08-30 20:57:45 +0500

    working on it

--- src/components/Web.jsx
+++ src/components/Web.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import hero from "./assets/refresh-hero-home-pk.png"
+import hero from "./assets/refresh-hero-home-pk.png";
 
 const Web = () => {
-
     return (
         <div className="web">
-            <div className="image">
-            </div>
-            <div className="input">
-                <h1 className="heading">It's the food and groceries you love, delivered</h1>
-                <div className="inpu">
-                    <input className="inp"></input>
+            <div className="hero">
+                <div className="input">
+                    <h1 className="heading">It's the food and groceries you love, delivered</h1>
+                    <div className="inputGroup">
+                        <input className="inp" placeholder="Your street and street number" />
+                        <button className="btn">Find food</button>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src={hero} alt="Hero" />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Web;
