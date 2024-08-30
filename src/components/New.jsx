867845a279d3657e0490e8a448ace5972fceb09e
M.BasilKhan <basilkhan1250@gmail.com>
2024-08-30 22:58:15 +0500

    working on it

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -10,10 +10,13 @@ const New = () => {
             <div className="container">
                 <img className="chef" src={chef}></img>
             </div>
-                <div className="add">
-                    <h2 className="add-text">List your restaurant or shop on foodpanda</h2>
-                    <p className="ad-text">Would you like millions of new customers to enjoy your amazing food and groceries? So would we!</p>
-                </div>
+            <div className="add">
+                <h2>List your restaurant or shop on foodpanda</h2>
+                <p className="add-text">Would you like millions of new customers to enjoy your amazing food and groceries? So would we!</p>
+                <p className="add-text">It's simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat!</p>
+                <p className="add-text">Interested? Let's start our partnership today!</p>
+                <button className="add-btn">Get started</button>
+            </div>
         </div>
     )
 }
